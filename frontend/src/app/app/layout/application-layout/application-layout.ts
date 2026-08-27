01a7b8c1a4aad2fdca5d180d4d9f590b3940f28e
kyliejurgs <kyliejurgs@gmail.com>
2026-08-27 10:51:58 -0400

    Add layout components

--- frontend/src/app/app/layout/application-layout/application-layout.ts
+++ frontend/src/app/app/layout/application-layout/application-layout.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
+import { LogoArea } from '../logo-area/logo-area';
+import { Header } from '../header/header';
+import { ActivityBar } from '../activity-bar/activity-bar';
+import { LeftPanel } from '../left-panel/left-panel';
+import { CenterArea } from '../center-area/center-area';
+import { RightPanel } from '../right-panel/right-panel';
+import { StatusBar } from '../status-bar/status-bar';
 
 @Component({
-  imports: [],
+  imports: [LogoArea, Header, ActivityBar, LeftPanel, CenterArea, RightPanel, StatusBar],
   selector: 'app-application-layout',
   styleUrl: './application-layout.css',
   templateUrl: './application-layout.html',
