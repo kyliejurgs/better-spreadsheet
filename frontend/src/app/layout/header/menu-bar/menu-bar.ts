c158b2003e30a3c20ff431c2f0539108a300acbe
kyliejurgs <kyliejurgs@gmail.com>
2026-08-30 17:47:49 -0400

    Implement placeholders and formatting for menu bar

--- frontend/src/app/layout/header/menu-bar/menu-bar.ts
+++ frontend/src/app/layout/header/menu-bar/menu-bar.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { TuiIcon } from '@taiga-ui/core';
 
 @Component({
-  imports: [],
+  imports: [TuiIcon],
   selector: 'app-menu-bar',
   styleUrl: './menu-bar.css',
   templateUrl: './menu-bar.html',
