bc3821e491ee50cbc5dedefb4ffe7c94b528f35f
kyliejurgs <kyliejurgs@gmail.com>
2026-08-30 10:07:15 -0400

    Add logo svg and reusable icon

---
+++ frontend/src/app/shared/logo/logo.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-logo',
+  styleUrl: './logo.css',
+  templateUrl: './logo.html',
+})
+export class Logo {}
