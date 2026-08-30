acd1fcb708ae5fec3df04d65a32d43ffcaca99f6
kyliejurgs <kyliejurgs@gmail.com>
2026-08-30 12:31:02 -0400

    Remove duplicate taiga

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,5 +2,5 @@ import { provideTaiga } from '@taiga-ui/core';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideTaiga(), provideTaiga()],
+  providers: [provideBrowserGlobalErrorListeners(), provideTaiga()],
 };
