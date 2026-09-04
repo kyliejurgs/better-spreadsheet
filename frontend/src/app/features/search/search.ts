566b902a04fcfb236ec61118b396cd09ed901ba2
kyliejurgs <kyliejurgs@gmail.com>
2026-09-04 10:56:44 -0400

    Format search and replace

--- frontend/src/app/features/search/search.ts
+++ frontend/src/app/features/search/search.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { TuiIcon, TuiTextfield } from '@taiga-ui/core';
 
@@ -10,18 +10,33 @@ import { TuiIcon, TuiTextfield } from '@taiga-ui/core';
 })
 export class Search {
   protected readonly searchText = signal('');
+  protected readonly replaceText = signal('');
+
   protected readonly matchCase = signal(false);
-  protected readonly matchWholeWord = signal(false);
+  protected readonly matchWholeValue = signal(false);
+  protected readonly replaceExpanded = signal(false);
 
   protected toggleMatchCase(): void {
     this.matchCase.update((value) => !value);
   }
 
-  protected toggleMatchWholeWord(): void {
-    this.matchWholeWord.update((value) => !value);
+  protected toggleMatchWholeValue(): void {
+    this.matchWholeValue.update((value) => !value);
+  }
+
+  protected toggleReplace(): void {
+    this.replaceExpanded.update((expanded) => !expanded);
   }
 
   protected clearSearch(): void {
     this.searchText.set('');
   }
+
+  protected clearReplace(): void {
+    this.replaceText.set('');
+  }
+
+  protected readonly canReplaceAll = computed(() => {
+    return this.searchText().length > 0 && this.replaceText().length > 0;
+  });
 }
