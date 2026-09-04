e185124d54cf04bef181007b73a1049ffc9fb35e
kyliejurgs <kyliejurgs@gmail.com>
2026-09-04 08:41:18 -0400

    Add search shell

--- frontend/src/app/features/search/search.ts
+++ frontend/src/app/features/search/search.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TuiIcon, TuiTextfield } from '@taiga-ui/core';
 
 @Component({
-  imports: [],
   selector: 'app-search',
-  styleUrl: './search.css',
+  imports: [FormsModule, TuiIcon, TuiTextfield],
   templateUrl: './search.html',
+  styleUrl: './search.css',
 })
-export class Search {}
+export class Search {
+  protected readonly searchText = signal('');
+  protected readonly matchCase = signal(false);
+  protected readonly matchWholeWord = signal(false);
+
+  protected toggleMatchCase(): void {
+    this.matchCase.update((value) => !value);
+  }
+
+  protected toggleMatchWholeWord(): void {
+    this.matchWholeWord.update((value) => !value);
+  }
+
+  protected clearSearch(): void {
+    this.searchText.set('');
+  }
+}
