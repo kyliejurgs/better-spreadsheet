fddc8c87485da37b07dc2c1d7712b8d472ab3fc9
kyliejurgs <kyliejurgs@gmail.com>
2026-09-08 08:51:39 -0400

    Apply theme to application

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,13 +1,17 @@
 import { TuiRoot } from '@taiga-ui/core';
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { ApplicationLayout } from './layout/application-layout/application-layout';
+import { ThemeService } from './core/theme/theme.service';
 
 @Component({
-  imports: [ApplicationLayout, TuiRoot, TuiRoot],
+  imports: [ApplicationLayout, TuiRoot],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
 export class App {
+  private readonly themeService = inject(ThemeService);
+
   protected readonly title = signal('frontend');
+  protected readonly resolvedTheme = this.themeService.resolvedTheme;
 }
